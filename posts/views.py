ecb9a7c27c880b836f89993afa4016167275cd90
Guillermo Ruiz A <gui.ruiz.alvarado@gmail.com>
2021-06-19 12:32:09 -0500

    Actualizamos codigos de secret-key y primera vista

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,30 @@
 """ Posts views. """
-#from django.shortcuts import render
-from django.http import HttpResponse
+
+from django.shortcuts import render
 # Create your views here.
+from datetime import datetime
+posts = [
+    {
+        'name':'Mont Blac',
+        'user':'Yesica Cortés',
+        'timestamp':datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/200/200/?image=1036'
+    },
+
+    {
+        'name': 'Via Láctea',
+        'user': 'C. Vander',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/200/200/?image=903',
+    },
+    {
+        'name': 'Nuevo auditorio',
+        'user': 'Thespianartist',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/200/200/?image=1076',    
+        }    
+]
 
 def list_posts(request):
     """ List existing posts"""
-    posts = [1,2,4]
-    return HttpResponse(str(posts))
+    return render(request,'feed.html')
